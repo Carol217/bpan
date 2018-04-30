aa7ccb3ca63c6e982fc7b36400947645dd36b18a
Valery Chen <cpan3@stuy.edu>
2018-04-30 19:28:54 -0400

    phrase freq works

--- book.py
+++ book.py
@@ -1,27 +1,1 @@
-'''
-team bpan
-Brian Leung and Carol Pan
-SoftDev2 pd7
-HW17- something about books
-2017-04-31
-'''
-
-from functools import reduce
-
-
-dracula = open('dracula.txt', 'r')
-draculist = dracula.read().replace("\r\n", " ").split(" ")
-#print draculist
-
-#find the frequency of a single word
-def word_freq(word):
-    lst = [1 for x in draculist if word.lower() == x.lower().strip(".?!,\'")]
-    return reduce ((lambda x,y: x+y), lst) 
-
-#find total frequency of a group of words
-
-
-
-#find most frequently occuring word
-
-print word_freq("dracula")
+'''team bpanBrian Leung and Carol PanSoftDev2 pd7HW17- Reductio ad Absurdum2017-04-31'''from functools import reducedracula = open('dracula.txt', 'r')draculist = dracula.read().replace("\"", "").replace("\n", " ").split(" ")#print draculist#find the frequency of a single worddef word_freq(word):    dl = [x[0 : x.index("'")] if "'" in x else x for x in draculist]    lst = [1 for x in dl if word.lower() == x.lower().strip(".?!,;")]    return reduce ((lambda x,y: x+y), lst) #find total frequency of a group of wordscount = -1def p_helper(phrase,x):    global count    #if the next word of the phrase is there    if phrase[count + 1] in x:        count += 1 #add to the count        #do not go out of bounds        if len(phrase) == count + 1:            count = -1            return 1        else:            return 0    #if the next word in phrase is NOT there    else:        count = -1        return 0def phrase_freq(phrase):    global count    count = -1    phrase = phrase.lower().split(" ")    lst = [p_helper(phrase,x.lower()) for x in draculist]    return reduce((lambda x,y: x+y), lst) #find most frequently occuring worddef top_word():    returnprint word_freq("Dracula")print phrase_freq("Dracula")print phrase_freq("Count Dracula")print phrase_freq("Do you know")
